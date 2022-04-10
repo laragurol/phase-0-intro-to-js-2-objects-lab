923d6e8ebbe1cd66f6c9675a7fb57f8cd048294c
Lara <i.laragurol@gmail.com>
2022-04-10 03:59:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const employee = { 
+    name: "Lara", 
+    streetAdress: "xyz" }; 
+    
+    function updateEmployeeWithKeyAndValue(obj, key, value) {
+        const newObj = { ...obj };
+      
+        newObj[key] = value;
+      
+        return newObj;
+      }
+      
+      function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+        obj[key] = value; 
+      
+        return obj;
+      }
+
+      function deleteFromEmployeeByKey(obj, key, value) {
+          const newObj = { ...obj };
+
+          delete newObj.key;
+
+          newObj[key] = value;
+
+          return newObj;
+      }
+
+      function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+
+          delete obj.key;
+          
+          obj[key] = value;
+
+          return obj;
+      }
 // Write your solution in this file!
